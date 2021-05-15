f8e8e97253e3cddc4de832a678b93665ce7b0ca2
Shekhar Wagh <shekharnwagh@gmail.com>
2021-05-15 20:47:03 +0530

    Only add HOST param when starting express server for dev environment

--- server.ts
+++ server.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import router from './src/routes/router';
+import Constants from './src/common/constants';
 import { logger } from './src/utils';
 
 const app = express();
@@ -10,6 +11,12 @@ app.use('/api', router);
 const PORT = process.env.PORT ? parseInt(process.env.PORT, 10) : 8080;
 const HOST = process.env.HOST ? process.env.HOST : 'localhost';
 
-app.listen(PORT, HOST, () => {
-    logger.info(`Started server on HOST: ${HOST}, at PORT: ${PORT}`);
-});
+if (process.env.APP_ENV && process.env.APP_ENV !== Constants.APP_ENV.DEVELOPMENT) {
+    app.listen(PORT, () => {
+        logger.info(`Started server on APP_ENV: ${process.env.APP_ENV}, at PORT: ${PORT}`);
+    });
+} else {
+    app.listen(PORT, HOST, () => {
+        logger.info(`Started server on HOST: ${HOST}, at PORT: ${PORT}`);
+    });
+}
